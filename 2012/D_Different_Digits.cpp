c0c8461130c8c3417fdbc553f8f5f5446c84d8f6
AlexPuenteJ <150400605+AlexPuenteJ@users.noreply.github.com>
2023-11-10 06:02:24 -0500

    Update D_Different_Digits.cpp

--- 2012/D_Different_Digits.cpp
+++ 2012/D_Different_Digits.cpp
@@ -1,16 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int f(int t){
+int f(int t){ //devuelve 1 si todos los dígitos de t son diferentes
     bitset<10>visited;
      
-    while(t!=0){
-        if(visited[t%10])return 0;
+    while(t!=0){//verifico dígito por dígito si hay repetidos
+        if(visited[t%10])return 0;//si visited[t%10] es > 0 hay repetidos y termina la funcion
         visited[t%10]=1;
         t/=10;    
     }
     
-    return 1;
+    return 1;//si nunca encontró repetidos, entonces no hay repetidos en el número
 }
 
 int main(){
@@ -19,11 +19,11 @@ int main(){
     c[0]=0;
     
     for(int i=1;i<=5000;i++)
-        c[i]=c[i-1]+f(i);
+        c[i]=c[i-1]+f(i);  //es una suma de prefijos 
     
     int a,b;
-    while(cin>>a>>b){
-        cout<<c[b]-c[a-1]<<"\n";
+    while(cin>>a>>b){//mientras siga recibiendo entrada de datos
+        cout<<c[b]-c[a-1]<<"\n";//es importante el salto de línea
     }
     
     return 0;
